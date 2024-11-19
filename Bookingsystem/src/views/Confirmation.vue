b6776be437270c1bed821b327cbd37852e1925ca
Trace <d.nguyen@live.dk>
2024-11-20 00:52:01 +0100

    changed data storage
    data now doesnt get sent to db immediately, saves in local storage instead until confirmation page is reached. more coming later

--- Bookingsystem/src/views/Confirmation.vue
+++ Bookingsystem/src/views/Confirmation.vue
@@ -1,10 +1,16 @@
 <script setup>
+import { ref } from 'vue';
+import { db } from '../firebase';
+import { ref as dbRef, set } from 'firebase/database';
+import Allan from '@/components/Trainers/Allan.vue';
 
 </script>
 
 
 <template>
-
+<div> 
+  <h1> Confirmation Page</h1>
+</div>
 </template>
 
 
